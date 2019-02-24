a2c919169cc1f02fefb85c69546ef58be2e66300
David Hallstrom <dhallstr@rams.colostate.edu>
2019-02-23 17:39:38 -0700

    Added namespaces

--- code/HandSignal.h
+++ code/HandSignal.h
@@ -6,17 +6,15 @@
 #include <vector>
 #include "Leap.h"
 
-using namespace Leap;
-
 class HandSignal {
     public:
-        HandSignal(const &vector<const FingerList> list) const;
-        bool matchesSignal(const FingerList &fingers, int &errorcode) const;
+        HandSignal(const &std::vector<const Leap::FingerList> list) const;
+        bool matchesSignal(const Leap::FingerList &fingers, int &errorcode) const;
         bool isValid();
         
     private:
         int fingers;
-        Finger::Type fingerTypes[20];
+        Leap::Finger::Type fingerTypes[20];
         float fingerLengths[20]
         int boneTypes[20][4];
         float boneStarts[20][4][3];
